768ab94c3184373614993bcfd357eae155197d7a
chunhodong <yhms4432@naver.com>
2022-09-12 21:24:25 +0900

    [feat] onApplicationEvent메소드로직수정
    속성검사로직수정

--- src/main/java/io/github/ki/detector/listener/EnvironmentPrepareListener.java
+++ src/main/java/io/github/ki/detector/listener/EnvironmentPrepareListener.java
@@ -21,7 +21,6 @@ public class EnvironmentPrepareListener implements ApplicationListener<Applicati
                 .findFirst().orElse(null);
 
 
-
         Map<String,Object> trackedPropertyMap = trackedMapPropertySource.getSource();
         Map<String, List<String>> map = DetectorParser.parsePropertyMap(trackedPropertyMap);
         if(map == null || map.isEmpty())return;;
@@ -29,8 +28,8 @@ public class EnvironmentPrepareListener implements ApplicationListener<Applicati
         map.entrySet().stream().forEach(e -> {
             Object object = trackedPropertyMap.get(e.getKey());
             if(object != null){
-                long count = e.getValue().stream().filter(s -> s.equals(object.toString())).count();
-                if(count > 0)throw new IllegalArgumentException("restrict property value");
+                boolean isContain = e.getValue().stream().anyMatch(s -> s.equals(object.toString()));
+                if(isContain)throw new IllegalArgumentException("not allowed ddl-auto property");
             }
         });
     }
