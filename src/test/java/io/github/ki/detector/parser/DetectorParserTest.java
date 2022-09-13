ebd64183a7d27fbede2b8b5680e2cd189a34dccd
chunhodong <yhms4432@naver.com>
2022-09-13 14:54:54 +0900

    [test] 테스트코드수정
    미사용코드제거

--- src/test/java/io/github/ki/detector/parser/DetectorParserTest.java
+++ src/test/java/io/github/ki/detector/parser/DetectorParserTest.java
@@ -50,27 +50,5 @@ public class DetectorParserTest {
 
     }
 
-    public static Map<String, String> parsePropertyMap(Map<String,Object> map){
-
-
-        Map<String,Object> filteredMap = map.entrySet()
-                .stream()
-                .filter(e -> e.getKey().indexOf(DetectorConstant.PREFIX.getValue()) != -1)
-                .collect(Collectors.toMap(e->e.getKey(),e->e.getValue()));
-
-        if(filteredMap.isEmpty())return Collections.EMPTY_MAP;
-        Map<String, String> result = new HashMap<>();
-        parseHibernateSyntax(filteredMap,result);
-
-        return result;
-    }
-
-    private static void parseHibernateSyntax( Map<String,Object> srcMap,Map<String, String> descMap){
-        OriginTrackedValue originTrackedValue = OriginTrackedValue.of(srcMap.get(DetectorConstant.HIBERNATE.getProperty()));
-
-        if(originTrackedValue == null)return;
-        if(originTrackedValue.getValue().toString() != Boolean.TRUE.toString())return;
-        descMap.put("spring.jpa.hibernate.ddl-auto","create");
-    }
 
 }
